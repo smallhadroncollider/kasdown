1ee8ee3fd67fa8ed37ce92837b8b7d7cd8483790
Mark Wales <mark@smallhadroncollider.com>
2020-01-15 13:15:40 +0000

    fix: fixes JS/HTML/CSS issue for iOS 6 support

--- templates/js.js
+++ templates/js.js
@@ -1,9 +1,33 @@
-((d) => {
-    document.addEventListener("click", e => {
-        let clicked = e.composedPath().find(el => el.tagName === "LI")
+// needs to support iOS 6
+(function (d) {
+    var toArr = function (arr) {
+        return Array.prototype.slice.call(arr)
+    };
 
-        if (clicked) {
-            clicked.classList.add("done");
-        }
+    var items = toArr(d.getElementsByTagName("li"));
+
+    items.forEach(function (current) {
+        var el, span;
+
+        p = d.createElement("p");
+        p.setAttribute("class", "clickable");
+
+        els = toArr(current.childNodes).filter(function (el) {
+            return el.tagName !== "UL";
+        });
+
+        current.insertBefore(p, current.firstChild);
+
+        els.forEach(function (el) {
+            p.appendChild(el);
+        });
+    })
+
+    var clickables = toArr(d.getElementsByClassName("clickable"));
+
+    clickables.forEach(function (current) {
+        current.addEventListener("click", function () {
+            current.setAttribute("class", "done");
+        });
     });
 })(document)
