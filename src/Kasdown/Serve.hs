2d851c494cfa5a0e840ccebb34620a95d86822e0
Mark Wales <mark@smallhadroncollider.com>
2020-01-15 11:29:44 +0000

    feat: shows local IP address

--- src/Kasdown/Serve.hs
+++ src/Kasdown/Serve.hs
@@ -5,14 +5,22 @@ module Kasdown.Serve
 import ClassyPrelude
 
 import Network.HTTP.Types       (status200)
+import Network.Info             (getNetworkInterfaces, ipv4)
 import Network.Wai              (Application, responseLBS)
 import Network.Wai.Handler.Warp (run)
 
 import Kasdown.CLI (message)
 
+getIP :: IO [Text]
+getIP = do
+    interfaces <- getNetworkInterfaces
+    pure . filter (/= "0.0.0.0") $ tshow . ipv4 <$> interfaces
+
 serve :: Int -> Text -> IO ()
 serve port content = do
-    message $ "Listening on port " <> tshow port <> "..."
+    ips <- getIP
+    let items = unlines $ ("    - http://" <>) . (<> ":" <> tshow port) <$> ips
+    message $ "Available on:\n" <> items
     run port (app content)
 
 app :: Text -> Application
